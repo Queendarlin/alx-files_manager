c86bd3cc476c3bb4c79f45c867ab287d081be002
Queendarlin <queendarlinnnamani@gmail.com>
2024-09-02 08:19:04 -0700

    Redis utils

--- utils/redis.js
+++ utils/redis.js
@@ -4,36 +4,36 @@ import redis from 'redis';
 import { promisify } from 'util';
 
 class RedisClient {
-    constructor() {
-        this.client = redis.createClient();
+  constructor () {
+    this.client = redis.createClient();
 
-        // Handle Redis client errors
-        this.client.on('error', (error) => {
-            console.error(`Redis client not connected to the server: ${error.message}`);
-        });
+    // Handle Redis client errors
+    this.client.on('error', (error) => {
+      console.error(`Redis client not connected to the server: ${error.message}`);
+    });
 
-        // Promisify Redis client methods for easier use with async/await
-        this.getAsync = promisify(this.client.get).bind(this.client);
-        this.setAsync = promisify(this.client.set).bind(this.client);
-        this.delAsync = promisify(this.client.del).bind(this.client);
-    }
+    // Promisify Redis client methods for easier use with async/await
+    this.getAsync = promisify(this.client.get).bind(this.client);
+    this.setAsync = promisify(this.client.set).bind(this.client);
+    this.delAsync = promisify(this.client.del).bind(this.client);
+  }
 
-    isAlive() {
-        return this.client.connected;
-    }
+  isAlive () {
+    return this.client.connected;
+  }
 
-    async get(key) {
-        return this.getAsync(key);
-    }
+  async get (key) {
+    return this.getAsync(key);
+  }
 
-    async set(key, value, duration) {
-        // Set the value with an expiration time in seconds
-        await this.setAsync(key, value, 'EX', duration);
-    }
+  async set (key, value, duration) {
+    // Set the value with an expiration time in seconds
+    await this.setAsync(key, value, 'EX', duration);
+  }
 
-    async del(key) {
-        return this.delAsync(key);
-    }
+  async del (key) {
+    return this.delAsync(key);
+  }
 }
 
 // Create and export an instance of RedisClient
